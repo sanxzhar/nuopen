02ee1bf39f56b5c11f2363bbc722599c7cca1617
Sanzhar <sanxzhar@gmail.com>
2024-10-19 11:14:47 +0500

    resolved

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import {LeftSideOrnekLayout, SectionOrnekLayout} from "./layouts";
 import {Sponsors} from "./sections";
 import { Rules } from "./sections";
 import OnlineOfflineTable from "./sections/OnlineOfflineTable/OnlineOfflineTable.tsx";
+import Promoting from "./sections/Promoting/Promoting.tsx";
+import Agenda from "./sections/Agenda/Agenda.tsx";
 function App() {
   return (
     <LeftSideOrnekLayout>
